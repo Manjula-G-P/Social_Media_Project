08f3fa82937f1dec8d5ada38ec74cdfc04f8b2ad
Manjula-G-P <manjula20@navgurukul.org>
2022-01-17 19:50:43 +0530

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express=require('express');
+const connectDB=require('./config/db');
+
 const app=express();
 
+//connect DataBase
+connectDB();
+
 app.get('/',(req,res)=>{
-    res.send("Hello World!")
+    res.send("API Running....");
 });
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen('3000',()=>{
     console.log('server started on port 3000');
